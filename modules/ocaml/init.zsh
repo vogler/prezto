bdecdd8b193087ab16478cf49be63417304d469b
Ralf Vogler <ralf.vogler@gmail.com>
2014-11-12 10:09:30 +0100

    opam: keep build files and generate .cmt for Merlin's :Locate

--- modules/ocaml/init.zsh
+++ modules/ocaml/init.zsh
@@ -12,4 +12,6 @@ fi
 
 # Initialize OPAM.
 . ~/.opam/opam-init/init.zsh > /dev/null 2> /dev/null || true
-
+# support Merlin's Locate on all libs
+export OPAMKEEPBUILDDIR=true
+export OCAMLPARAM="_,bin-annot=1"
