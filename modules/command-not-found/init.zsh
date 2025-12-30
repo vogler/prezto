b8b8fabd9241f91d77f8b4e22eb0a0c59de8b348
Ralf Vogler <ralf.vogler@gmail.com>
2025-12-30 15:53:48 +0100

    fix command-not-found location after zprezto update
    https://github.com/sorin-ionescu/prezto/commit/c945922b2268ca1959a3ed29368b1c21a07950c1

--- modules/command-not-found/init.zsh
+++ modules/command-not-found/init.zsh
@@ -17,7 +17,7 @@ elif [[ -s /usr/share/doc/pkgfile/command-not-found.zsh ]]; then
 elif [[ "$OSTYPE" =~ ^darwin ]] && (( $+commands[brew] )); then
   homebrew_repo=${HOMEBREW_REPOSITORY:-$commands[brew]:A:h:h/Library}
   # Look for handler in Homebrew core (as of >=4.6.12), then in Taps (< 4.6.12)
-  for hb_cnf_handler in "$homebrew_repo"/{Homebrew/command-not-found/handler.sh,Taps/homebrew/homebrew-command-not-found/handler.sh}; do
+  for hb_cnf_handler in "$homebrew_repo"/{Library/Homebrew/command-not-found/handler.sh,Taps/homebrew/homebrew-command-not-found/handler.sh}; do
     if [ -f "$hb_cnf_handler" ]; then
       source "$hb_cnf_handler"
       unset hb_cnf_handler homebrew_repo
